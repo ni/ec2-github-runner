faa67ae882e1f9936ea5adaceff00a250af060ba
hukirala <31215997+krlydm@users.noreply.github.com>
2021-12-16 13:55:48 +0100

    Run as process on Windows.

--- src/aws.js
+++ src/aws.js
@@ -20,7 +20,8 @@ function buildUserDataScript(githubRegistrationToken, label) {
         `mkdir ${i}; cd ${i}`,
         `Expand-Archive -Path ./../actions-runner-${config.input.ec2BaseOs}-${runnerVersion}.zip -DestinationPath $PWD`,
         'mkdir _work',
-        `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --name ${config.input.ec2BaseOs}-${label}-${i} --token ${githubRegistrationToken} --labels ${label} --unattended --runasservice`,
+        `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --name ${config.input.ec2BaseOs}-${label}-${i} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+        './run.cmd',
         'cd ..',
       );
     }
